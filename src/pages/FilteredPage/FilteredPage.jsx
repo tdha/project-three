5331bbc7a5e949101ea78a0c5666b600e4bfc471
Ton Ha <ton@tdha.co>
2024-03-26 14:27:49 +1000

    In progress

--- src/pages/FilteredPage/FilteredPage.jsx
+++ src/pages/FilteredPage/FilteredPage.jsx
@@ -1,28 +1,46 @@
 import React from "react";
 
-const FilteredPage = ({ fifteenMinute }) => {
-    console.log('this is FP', fifteenMinute);
 
-    // const fifteenMinuteDisplay = fifteenMinute.flatMap((property) => (
-    //     <div>{property.propertyData.address}</div>
-    // ));
-    
-    // const fifteenMinuteDisplay = fifteenMinute.map(property => (
-    //     <div>{property.propertyData.address}</div>
-    // ));
-    
-    const travelTimes = fifteenMinute.map(property => property.properties[0].travel_time);
-    console.log(travelTimes);
+const FilteredPage = ({ fifteenMinute, setFifteenMinute }) => {
+    console.log(fifteenMinute);
 
     return (
-        <>      
-        {fifteenMinute.map(property => 
-            <div>
-                {property.propertyData}
-            </div>)}
-        </>
-
+        <h1>
+         FilteredPage 
+         {console.log(fifteenMinute)};
+        </h1>
     )
 };
 
 export default FilteredPage;
+
+
+
+// import React from "react";
+
+// // const FilteredPage = ({ fifteenMinute }) => {
+// //     console.log('this is FP', fifteenMinute);
+
+// //     const fifteenMinuteDisplay = fifteenMinute.flatMap((property) => (
+// //         <div>{property.propertyData.address}</div>
+// //     ));
+    
+// //     // const fifteenMinuteDisplay = fifteenMinute.map(property => (
+// //     //     <div>{property.propertyData.address}</div>
+// //     // ));
+    
+// //     // const travelTimes = fifteenMinute.map(property => property.properties[0].travel_time);
+// //     // console.log(travelTimes);
+
+// //     return (
+// //         <>      
+// //         {fifteenMinute.map(property => 
+// //             <div>
+// //                 {property.propertyData}
+// //             </div>)}
+// //         </>
+
+// //     )
+// // };
+
+// export default FilteredPage;
