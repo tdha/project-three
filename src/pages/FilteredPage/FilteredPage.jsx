cf68dc15e7d64a2db99fa20be8a4aaca91aa52c0
David Burt <burt.david.j@gmail.com>
2024-03-26 15:26:55 +1100

    fixex prop

--- src/pages/FilteredPage/FilteredPage.jsx
+++ src/pages/FilteredPage/FilteredPage.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 
-const FilteredPage = ({ fifteenMinute }) => {
-    console.log('this is FP', fifteenMinute);
-
-    // const fifteenMinuteDisplay = fifteenMinute.flatMap((property) => (
-    //     <div>{property.propertyData.address}</div>
-    // ));
+const FilteredPage = ({ results  }) => {
+    
+ console.log(results);
+    const fifteenMinuteDisplay = results.flatMap((property) => (
+        <div>{property.propertyData.address}</div>
+    ));
     
     // const fifteenMinuteDisplay = fifteenMinute.map(property => (
     //     <div>{property.propertyData.address}</div>
     // ));
     
-    const travelTimes = fifteenMinute.map(property => property.properties[0].travel_time);
-    console.log(travelTimes);
+    // const travelTimes = fifteenMinute.map(property => property.properties[0].travel_time);
+    // console.log(travelTimes);
 
     return (
         <>      
-        {fifteenMinute.map(property => 
+        
             <div>
-                {property.propertyData}
-            </div>)}
+             {fifteenMinuteDisplay}
+            </div>
         </>
 
     )
